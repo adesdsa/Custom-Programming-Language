bbeb0ea0b81c16604b7958e4d17e3d4dfc705bb3
PR1NTFF <ofekamitay10@gmail.com>
2025-08-11 21:58:57 +0300

    docx: Document the TokenFactory

--- src/tokenizer/token_factory.h
+++ src/tokenizer/token_factory.h
@@ -9,10 +9,13 @@ class TokenFactory
 
     ~TokenFactory() = default;
 
+    // Will build a new token. 
+    // @return: The fully initialized token. 
     void CreateToken();
 
+    // Checks if file is in EOF.
+    // @return: True => , False => .
     bool IsFinishedReadingSRC();
-
 
  private:
     MappedSRC m_mappedSRC;
