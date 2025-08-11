ef5ef55dc64c969931abff013eacd7a5abd8f22b
PR1NTFF <ofekamitay10@gmail.com>
2025-08-11 20:45:29 +0300

    decx: Document MappedSRC class

--- src/tokenizer/mapped_src.h
+++ src/tokenizer/mapped_src.h
@@ -5,14 +5,29 @@
 class MappedSRC
 {
  public:
-    MappedSRC(const std::ifstream& fSrc);
+    MappedSRC(std::ifstream& fSrc);
 
     ~MappedSRC() = default;
 
-    char Get();
+    // Gets current character from the stream based on the index.
+    // @return: Current characters from the stream.
+    char Get() const;
+
+    // Read n characters from the stream.
+    // @param ulCharsToRead: Number of characters to read from the stream.
+    // @return: Current characters from the stream.
     std::string Read(size_t ulCharsToRead) noexcept;
-    int Seek(size_t ulCharsToSkip) noexcept;
-    int Rewind(size_t ulCharsToSkip) noexcept;
+
+    // Skip a single character from the stream by changing the stream index.
+    // @return: True => Index is not EOF, False => Otherwise.
+    bool Seek() noexcept;
+
+    // Go back a single character from the stream by changing the stream index.
+    // @return: True => Index is SOF, False => Otherwise.
+    bool Rewind() noexcept;
+
+    // Check if the stream index is at the end of the file.
+    // @return: True => Index is EOF, False => Otherwise.
     bool IsEOF() const;
 
 
