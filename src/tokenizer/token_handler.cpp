8398a4c6dd3d90fb57ff6beffd402bbfb59b847b
PR1NTFF <ofekamitay10@gmail.com>
2025-08-12 09:30:13 +0300

    feat: ProcessRelevantToken method

--- src/tokenizer/token_handler.cpp
+++ src/tokenizer/token_handler.cpp
@@ -11,3 +11,8 @@ bool TokenHandler::IsRelevantToken(const Token &token) const noexcept
 {
     return !(token.GetType() == k_triviaType|| token.GetType() == k_unknownType);
 }
+
+void TokenHandler::ProcessRelevantToken(const Token &token) noexcept
+{
+    this->m_streamTokens << token.GetLine() << " " << token.GetType() << " " << token.GetValue() << ",";
+}
