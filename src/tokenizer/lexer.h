c64fb51d0f353ec926aa1fb62b1355a220724e4a
PR1NTFF <ofekamitay10@gmail.com>
2025-08-12 15:15:21 +0300

    feat: Lexer layout

---
+++ src/tokenizer/lexer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "token_factory.h"
+#include "token_handler.h"
+
+constexpr const char* k_sInputFile = "tokens_in.txt";
+
+// Responsible for managing all Token entityies to convert src to stream of tokens
+class Lexer
+{
+public:
+    Lexer();
+    ~Lexer() = default;
+
+    // Get every token from the source file and push it to the stream
+    // @return: None.
+    void Tokenize();
+
+
+private:
+    TokenFactory m_tokenFactory;
+    TokenHandler m_tokenHandler;
+};
