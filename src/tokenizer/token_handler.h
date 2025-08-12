63e86e9e87dcae7eedd3890760896bef7bc2db77
PR1NTFF <ofekamitay10@gmail.com>
2025-08-12 09:07:44 +0300

    docx: Document TokenHandler

--- src/tokenizer/token_handler.h
+++ src/tokenizer/token_handler.h
@@ -4,19 +4,34 @@
 
 #include "token.h"
 
+// Responsible for filter tokens from input stream to output stream  
 class TokenHandler
 {
 public:
     TokenHandler() = default;
     ~TokenHandler() = default;
 
+    // Preform an action based on th token's relevance.
+    // @param token: Current token from stream to handle.
+    // @return: None.
     void HandleToken(Token& token) noexcept;
 
 
 private:
     std::ofstream m_streamTokens;
 
-    bool IsRelevantToken(Token& token) noexcept;
-    void SkipIrelevantToken(Token& token) noexcept;
-    void ProcessRelevantToken(Token& token) noexcept;
+    // Checks for token relevance.
+    // @param token: Current token from stream to check it relevance.
+    // @return: True => Token is relevant (not a trivia/unknown type token), False => Otherwise.
+    bool IsRelevantToken(const Token& token) const noexcept;
+
+    // Ignore irrelevant tokens.
+    // @param token: Current token from stream to handle.
+    // @return: None.
+    void SkipIrelevantToken(const Token& token) const noexcept;
+
+    // Adds the relevant tokens to the tokens stream.
+    // @param token: Current token from stream to handle.
+    // @return: None.
+    void ProcessRelevantToken(const Token& token) noexcept;
 };
