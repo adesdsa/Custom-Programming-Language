9e04eee1b8d5bcc68987c6eb8b758a82a35c5efc
PR1NTFF <ofekamitay10@gmail.com>
2025-08-21 15:43:51 +0300

    debug: Fix paths for testing

--- tests/lexer_test.cpp
+++ tests/lexer_test.cpp
@@ -2,9 +2,17 @@
 
 #include "tokenizer/lexer.h"
 
-
+// TEST: Empty source file.
+// INPUT: Empty source file.
+// EXPECTED: No tokens created.
 TEST(LexerTest, EmptyInput) 
 {
+    std::ofstream streamSrcFile(k_sInputFile);
+    ASSERT_TRUE(streamSrcFile.is_open());
+
+    streamSrcFile.clear();
+    streamSrcFile.close();
+
     Lexer lexer;
     lexer.Tokenize();
 
