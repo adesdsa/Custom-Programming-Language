7eb74d4b3cc066d94df3fdc858e32005429288dc
PR1NTFF <ofekamitay10@gmail.com>
2025-08-21 12:44:08 +0300

    feat: Add test for emptyinput

--- tests/lexer_test.cpp
+++ tests/lexer_test.cpp
@@ -1,1 +1,19 @@
-//
+#include <gtest/gtest.h>
+
+#include "tokenizer/lexer.h"
+
+
+TEST(LexerTest, EmptyInput) 
+{
+    Lexer lexer;
+    lexer.Tokenize();
+
+    std::ifstream streamOutputFile(k_sOutputFile);
+    ASSERT_TRUE(streamOutputFile.is_open());
+
+    std::string tokens;
+    std::getline(streamOutputFile, tokens);
+    streamOutputFile.close();
+
+    EXPECT_TRUE(tokens.empty());
+}
