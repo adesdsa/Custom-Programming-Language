591eb632e420f54bf8bb57188277a7c9138df74a
PR1NTFF <ofekamitay10@gmail.com>
2025-08-25 09:28:34 +0300

    debug: Remove cahche files

--- build/unit_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/Cyber_User/Magshimim/Final Project/Custom-Programming-Language/build/unit_tests[1]_tests.cmake")
-  include("C:/Users/Cyber_User/Magshimim/Final Project/Custom-Programming-Language/build/unit_tests[1]_tests.cmake")
-else()
-  add_test(unit_tests_NOT_BUILT unit_tests_NOT_BUILT)
-endif()
--- build/unit_tests[1]_tests.cmake
+++
@@ -1,7 +0,0 @@
-add_test([=[LexerTest.EmptyInput]=]  [==[C:/Users/Cyber_User/Magshimim/Final Project/Custom-Programming-Language/build/Debug/unit_tests.exe]==] [==[--gtest_filter=LexerTest.EmptyInput]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[LexerTest.EmptyInput]=]  PROPERTIES WORKING_DIRECTORY [==[C:/Users/Cyber_User/Magshimim/Final Project/Custom-Programming-Language/build]==] SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-add_test([=[LexerTest.OnlyTriviaTokens]=]  [==[C:/Users/Cyber_User/Magshimim/Final Project/Custom-Programming-Language/build/Debug/unit_tests.exe]==] [==[--gtest_filter=LexerTest.OnlyTriviaTokens]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[LexerTest.OnlyTriviaTokens]=]  PROPERTIES WORKING_DIRECTORY [==[C:/Users/Cyber_User/Magshimim/Final Project/Custom-Programming-Language/build]==] SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-add_test([=[LexerTest.SmallCodeExample]=]  [==[C:/Users/Cyber_User/Magshimim/Final Project/Custom-Programming-Language/build/Debug/unit_tests.exe]==] [==[--gtest_filter=LexerTest.SmallCodeExample]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[LexerTest.SmallCodeExample]=]  PROPERTIES WORKING_DIRECTORY [==[C:/Users/Cyber_User/Magshimim/Final Project/Custom-Programming-Language/build]==] SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  unit_tests_TESTS LexerTest.EmptyInput LexerTest.OnlyTriviaTokens LexerTest.SmallCodeExample)
